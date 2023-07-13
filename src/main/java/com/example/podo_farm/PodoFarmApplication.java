272d420af1324eb4c657e08b158574453fc4c6f2
JongHoWoo <jonghowoo33@gmail.com>
2023-07-13 10:11:22 +0900

    기본설정

---
+++ src/main/java/com/example/podo_farm/PodoFarmApplication.java
@@ -0,0 +1,13 @@
+package com.example.podo_farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodoFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodoFarmApplication.class, args);
+	}
+
+}
