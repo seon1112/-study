b762436e630d1a04e6423d2582d5af553d7e7f57
Woo-Jongho <jonghowoo33@gmail.com>
2023-07-14 09:57:05 +0900

    AccoutVO 컬럼값 입력: 우종호

--- podo_farm/src/main/java/com/example/demo/vo/AccountVO.java
+++ podo_farm/src/main/java/com/example/demo/vo/AccountVO.java
@@ -8,4 +8,10 @@ import lombok.Data;
 @Data
 public class AccountVO {
     private int a_no;
+    private String a_nick;
+    private String a_name;
+    private String a_gender;
+    private String a_email;
+    private String a_img;
+    private Date a_regdate;
 }
