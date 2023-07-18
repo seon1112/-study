c03b06d7921d520d8e261a86f565c5a8c36c04b2
Woo-JongHo <jonghowoo33@gmail.com>
2023-07-18 16:42:52 +0900

    DAO 변경

--- podo_farm/src/main/java/com/example/demo/dao/AccountDAO.java
+++ podo_farm/src/main/java/com/example/demo/dao/AccountDAO.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class AccountDAO {
+
+    public AccountVO findMyPage(int a_no){
+        return DBManager.getProfile(a_no);
+    }
     public int updateProfileImg(AccountVO a){
         return DBManager.updateProfile(a);
     }
